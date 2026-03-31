adc5f6ce38d6400b11a6276a8d579614e95bac5f
vgaudel <vgaudel@gmail.com>
2026-03-31 09:01:23 +0200

    supression imports inutiles

--- springcore/src/main/java/fr/dawan/springcore/AppConfig.java
+++ springcore/src/main/java/fr/dawan/springcore/AppConfig.java
@@ -1,14 +1,12 @@
 package fr.dawan.springcore;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
-import fr.dawan.springcore.beans.ArticleMapper;
-import fr.dawan.springcore.beans.ArticleRepository;
-import fr.dawan.springcore.beans.ArticleService;
+
+
 import fr.dawan.springcore.beans.EmployeMapper;
 
 @Configuration
